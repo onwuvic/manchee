6c034bf731870e2a4abf3d71a535e22bc98fa216
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-10-13 11:54:57 +0100

    add user dto and entity

---
+++ src/modules/users/entities/user.entity.ts
@@ -0,0 +1,70 @@
+import { Table, Column, Model, DataType, DefaultScope, Scopes, BeforeCreate } from 'sequelize-typescript';
+import * as bcrypt from 'bcrypt';
+
+@DefaultScope(() => ({
+    attributes: {
+        exclude: ['password']
+    }
+}))
+@Scopes(() => ({
+    withPassword: {}
+}))
+@Table
+export class User extends Model<User> {
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    firstName: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    lastName: string;
+
+    @Column({
+        type: DataType.STRING,
+        unique: true,
+        allowNull: false,
+    })
+    email: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    password: string;
+
+    @Column({
+        type: DataType.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    })
+    isVerify: boolean;
+
+    @Column({
+        type: DataType.ENUM,
+        values: ['male', 'female'],
+        allowNull: false,
+    })
+    gender: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: true
+    })
+    resetPasswordToken: string;
+
+    @Column({
+        type: DataType.DATE,
+        allowNull: true
+    })
+    resetPasswordExpires: number;
+
+    @BeforeCreate
+    static async hashPassword(user: User) {
+        user.password = await bcrypt.hash(user.password, 10);
+        user.gender.toLowerCase();
+    }
+}
