2240870b87f64b3f5ebb080036f6fe538272b64d
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-10-13 09:35:29 +0100

    setup sequelize and db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DEVELOPMENT, TEST, PRODUCTION } from './core/constants';
+import { databaseConfig } from './core/database/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRootAsync({
+      useFactory: () => {
+        let config;
+        switch (process.env.NODE_ENV) {
+          case DEVELOPMENT:
+            config = databaseConfig.development;
+            break;
+          case TEST:
+            config = databaseConfig.test;
+            break;
+          case PRODUCTION:
+            config = databaseConfig.production;
+            break;
+          default:
+            config = databaseConfig.development;
+        }
+        return { 
+          ...config, 
+          models: []
+        }
+      },
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
