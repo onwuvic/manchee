468f28408bb0ae777183681e7ccfd28ff70c5aed
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-10-13 09:48:15 +0100

    set global config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
+
 import { AppModule } from './app.module';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // endpoint prefix
+  app.setGlobalPrefix('api/v1');
+  // handle all user input validation globally
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
