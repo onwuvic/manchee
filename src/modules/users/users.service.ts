e0ed7951818041943d43aa6784074843b806a6ab
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-10-13 12:08:42 +0100

    add user service method

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from './entities/user.entity';
+import { UserDto } from './dto/user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectModel(User) private userRepository: typeof User) {}
+
+    public async create(user: UserDto): Promise<User> {
+        return await this.userRepository.create<User>(user);
+    }
+
+    public async findOneByEmail(email: string): Promise<User> {
+        return await this.userRepository.scope('withPassword').findOne<User>({ where: { email } });
+    }
+
+    public async findOneById(id: number): Promise<User> {
+        return await this.userRepository.findOne<User>({ where: { id } });
+    }
+}
