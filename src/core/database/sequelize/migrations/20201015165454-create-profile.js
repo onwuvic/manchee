2d444521b5d68850eba1b5c609b1fa5c1a38e559
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-10-15 19:52:19 +0100

    add profile migration details

--- src/core/database/sequelize/migrations/20201015165454-create-profile.js
+++ src/core/database/sequelize/migrations/20201015165454-create-profile.js
@@ -1,21 +1,48 @@
-'use strict';
-
+/* eslint-disable no-unused-vars */
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Profiles', {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      unique: true,
+      primaryKey: true,
+    },
+    avatarImage: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    coverImage: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    bio: {
+      type: Sequelize.TEXT,
+      allowNull: true
+    },
+    userId: {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Users',
+        key: 'id',
+        as: 'user',
+      },
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    deletedAt: {
+      allowNull: true,
+      type: Sequelize.DATE
+    },
+  }),
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  down: (queryInterface) => queryInterface.dropTable('Profiles')
 };
