e3b91536dfd938fdcb7d697eab4b0dcc9349d092
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-10-15 18:01:57 +0100

    update profile module file

--- src/modules/profiles/profiles.module.ts
+++ src/modules/profiles/profiles.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Profile } from './entities/profile.entity';
 import { ProfilesService } from './profiles.service';
 
 @Module({
-  providers: [ProfilesService]
+  imports: [SequelizeModule.forFeature([Profile])],
+  providers: [ProfilesService],
+  exports: [ProfilesService]
 })
 export class ProfilesModule {}
