74b8791d7e2d798c145ab7fa7b8a56a9f3dd294a
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-10-15 19:53:44 +0100

    add findAll and delete user to controller

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, UseGuards, Request, Get } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private userService: UsersService) {}
+
+    @Get()
+    async findAll(): Promise<User[]> {
+        return this.userService.findAll();
+    }
+
+    @UseGuards(AuthGuard('jwt'))
+    @Delete()
+    async delete(@Request() req): Promise<string> {
+        return this.userService.deleteById(req.user.id);
+    }
+}
