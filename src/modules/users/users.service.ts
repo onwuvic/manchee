8d82602262d69c07e1c6eaa231fa0e0704ad307f
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-10-15 19:53:56 +0100

    add findAll and delete user to service

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,28 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { Sequelize } from 'sequelize-typescript';
 import { User } from './entities/user.entity';
 import { UserDto } from './dto/user.dto';
+import { ProfilesService } from '../profiles/profiles.service';
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectModel(User) private userRepository: typeof User) {}
+    constructor(
+        @InjectModel(User) 
+        private userRepository: typeof User,
+        private sequelize: Sequelize,
+        private profilesService: ProfilesService
+    ) {}
 
-    public async create(user: UserDto): Promise<User> {
+    async create(user: UserDto): Promise<User> {
         // start a transaction
-        // create a user
-        // once user is created get the user Id
-        // create a profile with user id
-        // find the user with id and eager load the profile
+        const transaction = await this.sequelize.transaction();
+        try {
+            // create a user
+            const createdUser = await this.userRepository.create<User>(user, { transaction });
+            // once user is created get the user Id
+            // create a profile with user id
+            // find the user with id and eager load the profile
 
-        // profile migration, profile entity, profile service
-        return await this.userRepository.create<User>(user);
+            // profile migration, profile entity, profile service
+            await transaction.commit();
+            return createdUser; 
+        } catch (error) {
+            await transaction.rollback();
+            throw new InternalServerErrorException('Error creating user account. Try again later');
+        }  
     }
 
-    public async findOneByEmail(email: string): Promise<User> {
+    async findOneByEmail(email: string): Promise<User> {
         return await this.userRepository.scope('withPassword').findOne<User>({ where: { email } });
     }
 
-    public async findOneById(id: number): Promise<User> {
+    async findOneById(id: number): Promise<User> {
         return await this.userRepository.findOne<User>({ where: { id } });
+    }
+
+    async deleteById(id: number): Promise<string> {
+        await this.userRepository.destroy({ where: { id }});
+        return 'Deleted Successfully';
+    }
+
+    async findAll(): Promise<User[]> {
+        return this.userRepository.findAll();
     }
 }
