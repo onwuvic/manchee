c328a587551d7cef0b24d0e1a072f4114b4c8e3c
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-10-16 07:48:03 +0100

    add create profile service

--- src/modules/profiles/profiles.service.ts
+++ src/modules/profiles/profiles.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Profile } from './entities/profile.entity';
 
 @Injectable()
-export class ProfilesService {}
+export class ProfilesService {
+    constructor(@InjectModel(Profile) private profileRepository: typeof Profile) {}
+
+    async create(userId, transaction ): Promise<Profile> {
+        return await this.profileRepository.create<Profile>({ userId }, { transaction });
+    }
+}
