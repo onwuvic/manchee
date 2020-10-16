f7c1cdaf5419d7c52505c412486fca24ee437c92
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-10-16 07:47:32 +0100

    import profile module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,9 +3,13 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './entities/user.entity';
+import { ProfilesModule } from '../profiles/profiles.module';
 
 @Module({
-  imports: [SequelizeModule.forFeature([User])],
+  imports: [
+    SequelizeModule.forFeature([User]),
+    ProfilesModule
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService]
