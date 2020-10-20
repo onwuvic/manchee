9fbc414a33c1c8e5c7398fcd1c754d7a9e758aa6
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-10-20 14:43:38 +0100

    add friends module data

--- src/modules/friends/friends.module.ts
+++ src/modules/friends/friends.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { FriendsService } from './friends.service';
+import { FriendsController } from './friends.controller';
+import { Friend } from './entities/friend.entity';
+import { UsersModule } from '../users/users.module';
 
-@Module({})
+@Module({
+  imports: [
+    SequelizeModule.forFeature([Friend]),
+    UsersModule
+  ],
+  providers: [FriendsService],
+  controllers: [FriendsController]
+})
 export class FriendsModule {}
