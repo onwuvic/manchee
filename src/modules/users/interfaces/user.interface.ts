8c2dc3894035673f38cea5d682c47f75cb34b881
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-10-20 15:26:21 +0100

    working in progress interface

---
+++ src/modules/users/interfaces/user.interface.ts
@@ -0,0 +1,10 @@
+export interface Friends {
+    id: number;
+    type: string;
+    userId: number;
+    subjectId: number;
+    subjectType: string;
+    createdAt: string;
+    updatedAt: string;
+}
+
