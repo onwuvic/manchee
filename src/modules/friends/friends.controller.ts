d688628451e4c1f680d39c087b597e0a52b8bd9e
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-10-20 14:42:31 +0100

    add friends endpoints

---
+++ src/modules/friends/friends.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Post, UseGuards, Request, Get, Body, Put } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { FriendsService } from './friends.service';
+import { ID } from '../../core/validation/validation.dto';
+
+@Controller('friends')
+export class FriendsController {
+    constructor(private friendService: FriendsService) {}
+
+    @UseGuards(AuthGuard('jwt'))
+    @Post('sends')
+    async sendRequest(@Body() body: ID, @Request() req): Promise<any> {
+        return await this.friendService.sendRequest(req.user.id, body.id);
+    }
+
+    @UseGuards(AuthGuard('jwt'))
+    @Get()
+    async getAllFriends(@Request() req): Promise<any> {
+        return await this.friendService.allFriend(req.user.id);
+    }
+
+    @UseGuards(AuthGuard('jwt'))
+    @Put('accepts')
+    async acceptRequest(@Body() body: ID, @Request() req) {
+        return await this.friendService.acceptRequest(req.user.id, body.id);
+    }
+
+    // async rejectRequest() {
+
+    // }
+}
