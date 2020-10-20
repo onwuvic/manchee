89c273515d5b32770d3c00101a21078fb3c30453
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-10-20 14:24:35 +0100

    add friends methods functionalists

---
+++ src/modules/friends/friends.service.ts
@@ -0,0 +1,56 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Op } from 'sequelize';
+import { Friend } from './entities/friend.entity';
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class FriendsService {
+    constructor(
+        @InjectModel(Friend) 
+        private friendRepository: typeof Friend,
+        private userService: UsersService
+    ) {}
+
+    async sendRequest(authId, receiverId): Promise<any> {
+        // user should not send request to themselves
+        if (receiverId == authId) {
+           throw new BadRequestException('Can not send request to yourself');
+        }
+        // if the relationship exist don't, send message already sent
+        const request = await this.friendRepository.findOne({
+            where: { 
+                [Op.or]: [
+                    {receiverId, senderId: authId},
+                    {receiverId: authId, senderId: receiverId}
+                ]
+            }
+        });
+        if (request) {
+            throw new BadRequestException('Request already exist');
+        }
+        return await this.friendRepository.create({receiverId, senderId: authId});
+    }
+
+    async allFriend(userId): Promise<any> {
+        return await this.userService.findAllFriends(userId);
+    }
+
+    async acceptRequest(authId, senderId): Promise<any> {
+        // find the friend request where auth user is the reciever and the sender is another user
+        const request = await this.friendRepository.findOne({
+            where: { receiverId: authId, senderId }
+        });
+
+        if (!request) {
+            throw new BadRequestException('Request does not exist');
+        }
+
+        await request.update({ status: 'ACCEPTED'});
+        return 'Friend request accepted';
+    }
+
+    // async rejectRequest() {
+
+    // }
+}
