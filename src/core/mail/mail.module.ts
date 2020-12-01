a6522b26b32a0ecc187c0ba0a564bf7f1ebf1143
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-12-01 20:04:31 +0100

    create mail module

--- src/core/mail/mail.module.ts
+++ src/core/mail/mail.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { SendGridModule } from "@anchan828/nest-sendgrid";
+import { MailService } from './mail.service';
 
-@Module({})
+@Module({
+    imports: [
+        SendGridModule.forRoot({
+          apikey: process.env.SENDGRID_API_KEY,
+        }),
+      ],
+    providers: [MailService],
+    exports: [MailService]
+})
 export class MailModule {}
