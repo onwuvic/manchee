fcd88632ce1549150d754fcd8c629670f3e126d0
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-12-04 21:49:02 +0100

    reset password endpoint controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post, UseGuards, Request, Param, Get, Put } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { IsEmailDto, VerifyToken } from '../../core/validation/validation.dto';
+import { IsEmailDto, VerifyToken, Token, Password } from '../../core/validation/validation.dto';
 import { IsExist } from '../../core/guards/isExist.guard';
 import { UserDto } from '../users/dto/user.dto';
 import { AuthService } from './auth.service';
@@ -29,5 +29,10 @@ export class AuthController {
     @Put('forget/password')
     async sendResetPassword(@Body() body: IsEmailDto): Promise<any> {
         return await this.authService.sendResetPassword(body.email);
+    }
+
+    @Put('reset/password/:token')
+    async resetPassword(@Param() params: Token, @Body() body: Password): Promise<any> {
+        return await this.authService.resetPassword(params.token, body.password);
     }
 }
