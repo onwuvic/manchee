80963720f308d08fe025cb5f62dc4f3e8d1bcf7b
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-12-07 05:50:33 +0100

    add date of birth to seeders

--- src/core/database/sequelize/seeders/20201019154534-demo-user.js
+++ src/core/database/sequelize/seeders/20201019154534-demo-user.js
@@ -12,6 +12,7 @@ module.exports = {
       password: bcrypt.hashSync('password', 10),
       isVerify: true,
       gender: 'female',
+      dateOfBirth: faker.date.past(20, '2002-12-06T20:02:56+01:00'),
       createdAt: new Date(),
       updatedAt: new Date()
     }];
@@ -24,6 +25,7 @@ module.exports = {
         password: bcrypt.hashSync('password', 10),
         isVerify: true,
         gender: faker.random.arrayElement(gender),
+        dateOfBirth: faker.date.past(30, '2002-12-06T20:02:56+01:00'),
         createdAt: new Date(),
         updatedAt: new Date()
       };
