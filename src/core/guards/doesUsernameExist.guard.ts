9b565fe8f4a42ed22cc9205d878ff579805b5676
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-12-08 23:02:32 +0100

    create unique username guard

---
+++ src/core/guards/doesUsernameExist.guard.ts
@@ -0,0 +1,24 @@
+import { CanActivate, ExecutionContext, Injectable, ForbiddenException } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+import { UsersService } from '../../modules/users/users.service';
+
+@Injectable()
+export class DoesUsernameExist implements CanActivate {
+    constructor(private readonly userService: UsersService) { }
+
+    canActivate(
+        context: ExecutionContext,
+    ): boolean | Promise<boolean> | Observable<boolean> {
+        const request = context.switchToHttp().getRequest();
+        return this.validateRequest(request);
+    }
+
+    async validateRequest(request) {
+        const isExist = await this.userService.findUserByUsername(request.body.username);
+        if (isExist) {
+            throw new ForbiddenException('This username already exist');
+        }
+        return true;
+    }
+}
