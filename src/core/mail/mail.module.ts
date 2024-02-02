bfffa46f473f46193768dea6097946dd4c9a9cee
Onwuzor Victor <onwuzorvictor@outlook.com>
2024-02-02 13:43:49 +0100

    feat: add queue module

--- src/core/mail/mail.module.ts
+++ src/core/mail/mail.module.ts
@@ -1,21 +1,19 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
-import { BullModule } from '@nestjs/bull';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 // import { SendGridModule } from "@anchan828/nest-sendgrid";
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
-import { MailQueueConsumer } from './queue/mail.consumers';
+import { QueueModule } from 'core/queue/queue.module';
+
+import { MailQueueConsumer } from './consumers/mail.consumers';
 
 @Module({
   imports: [
     // SendGridModule.forRoot({
     //   apikey: process.env.SENDGRID_API_KEY,
     // }),
-    BullModule.registerQueueAsync({
-      name: 'mailSending',
-    }),
     MailerModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
@@ -27,8 +25,9 @@ import { MailQueueConsumer } from './queue/mail.consumers';
       }),
       inject: [ConfigService],
     }),
+    QueueModule,
   ],
   providers: [MailQueueConsumer],
-  exports: [BullModule],
+  exports: [QueueModule],
 })
 export class MailModule {}
