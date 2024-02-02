6a451376fd41a1bf7219187f7d0ab9c89edbe7a3
Onwuzor Victor <onwuzorvictor@outlook.com>
2024-02-02 12:30:46 +0100

    feat: add mail and general config to config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import mailConfig from 'core/mail/mail.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ProfilesModule } from './modules/profiles/profiles.module';
 import { FriendsModule } from './modules/friends/friends.module';
+import configuration from 'core/config/configuration';
 
 import databaseConfig from 'core/database/database.config';
 import { DatabaseModule } from 'core/database/database.module';
@@ -14,7 +16,7 @@ import { DatabaseModule } from 'core/database/database.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [databaseConfig],
+      load: [databaseConfig, mailConfig, configuration],
     }),
     DatabaseModule,
     UsersModule,
