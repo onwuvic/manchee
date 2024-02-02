49149a2189191f6c791112dc8ae06089feaac1b3
Onwuzor Victor <onwuzorvictor@outlook.com>
2024-02-02 11:58:23 +0100

    feat: add database configuration files

--- src/core/database/database.config.ts
+++ src/core/database/database.config.ts
@@ -1,31 +1,51 @@
-import * as dotenv from 'dotenv';
+import { registerAs } from '@nestjs/config';
+import {
+  Environment,
+  IDatabaseConfigAttributes,
+} from 'core/database/interface/dbConfig.interface';
 
-import { IDatabaseConfig } from './interface/dbConfig.interface';
+export default registerAs('databaseConfig', () => {
+  let config: IDatabaseConfigAttributes;
+  const development = {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME_DEVELOPMENT,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    type: process.env.DB_DIALECT,
+  };
+  const test = {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    type: process.env.DB_DIALECT,
+  };
+  const production = {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME_PRODUCTION,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    type: process.env.DB_DIALECT,
+  };
 
-dotenv.config();
-
-export const databaseConfig: IDatabaseConfig = {
-    development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME_DEVELOPMENT,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT,
-    },
-    test: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME_TEST,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT,
-    },
-    production: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME_PRODUCTION,
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-    },
-};
+  switch (process.env.NODE_ENV) {
+    case Environment.DEVELOPMENT:
+      config = development;
+      break;
+    case Environment.TEST:
+      config = test;
+      break;
+    case Environment.PRODUCTION:
+      config = production;
+      break;
+    default:
+      config = development;
+  }
+  return config;
+});
