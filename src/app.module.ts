fe0197fc6049d8c9a1dabfa281544d11050fdf21
Onwuzor Victor <onwuzorvictor@outlook.com>
2024-02-02 12:39:15 +0100

    feat: add queue with bull

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,29 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bull';
 import { ConfigModule } from '@nestjs/config';
 
 import mailConfig from 'core/mail/mail.config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './modules/users/users.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { ProfilesModule } from './modules/profiles/profiles.module';
-import { FriendsModule } from './modules/friends/friends.module';
+import { AuthModule } from 'modules/auth/auth.module';
 import configuration from 'core/config/configuration';
-
+import { UsersModule } from 'modules/users/users.module';
 import databaseConfig from 'core/database/database.config';
 import { DatabaseModule } from 'core/database/database.module';
+import { FriendsModule } from 'modules/friends/friends.module';
+import { ProfilesModule } from 'modules/profiles/profiles.module';
+
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [databaseConfig, mailConfig, configuration],
+    }),
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
     }),
     DatabaseModule,
     UsersModule,
