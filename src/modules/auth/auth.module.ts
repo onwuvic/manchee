debf36fdb188416ba2158258c76afb0255a7cdc3
Onwuzor Victor <onwuzorvictor@outlook.com>
2024-02-02 12:21:58 +0100

    feat: refactor jwt to use nest config

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,30 +1,33 @@
 import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { MailModule } from 'core/mail/mail.module';
+import { UsersModule } from 'modules/users/users.module';
+import { SecurityService } from 'core/services/security/security.service';
+
 import { AuthService } from './auth.service';
-import { UsersModule } from '../users/users.module';
-import { SecurityService } from '../../core/services/security/security.service';
-import { LocalStrategy } from './local.strategy';
 import { JwtStrategy } from './jwt.strategy';
-import { MailModule } from '../../core/mail/mail.module';
+import { LocalStrategy } from './local.strategy';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
-    JwtModule.register({
-      secret: process.env.JWTKEY,
-      signOptions: { expiresIn: process.env.TOKEN_EXPIRATION },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('jwtSecret'),
+        signOptions: { expiresIn: configService.get('jwtExpToken') },
+      }),
+      inject: [ConfigService],
     }),
-    MailModule
+    MailModule,
+    ConfigModule,
   ],
   controllers: [AuthController],
-  providers: [
-    AuthService, 
-    SecurityService, 
-    LocalStrategy,
-    JwtStrategy
-  ]
+  providers: [AuthService, SecurityService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -1,25 +1,31 @@
+import { ConfigService } from '@nestjs/config';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 
-import { UsersService } from '../users/users.service';
+import { UsersService } from 'modules/users/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(private readonly userService: UsersService) {
-        super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            ignoreExpiration: false,
-            secretOrKey: process.env.JWTKEY,
-        });
-    }
+  constructor(
+    private readonly userService: UsersService,
+    private configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('jwtSecret'),
+    });
+  }
 
-    async validate(payload: any) {
-        // check if user in the token actually exist
-        const user = await this.userService.findUserById(payload.id);
-        if (!user) {
-            throw new UnauthorizedException('You are not authorized to perform this operation');
-        }
-        return payload;
+  async validate(payload: any) {
+    // check if user in the token actually exist
+    const user = await this.userService.findUserById(payload.id);
+    if (!user) {
+      throw new UnauthorizedException(
+        'You are not authorized to perform this operation',
+      );
     }
+    return payload;
+  }
 }
