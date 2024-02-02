c709aa4af3dfdf631bef2656c2c4c3bb53ce0003
Onwuzor Victor <onwuzorvictor@outlook.com>
2024-02-02 12:20:35 +0100

    feat: use nest config package

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import * as dotenv from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 import { AppModule } from './app.module';
 
-dotenv.config();
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
   // endpoint prefix
   app.setGlobalPrefix('api/v1');
   // handle all user input validation globally
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(process.env.PORT || 4000);
+  await app.listen(configService.get('PORT') || 4000);
 }
 bootstrap();
